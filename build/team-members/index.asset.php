052acf116cf97eb16d5fb8ab52e6fa55bf29f2f9
Nayem Miah <naeemislam13790@gmail.com>
2025-04-07 17:31:33 +0600

    'commited'

--- build/team-members/index.asset.php
+++ build/team-members/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '25565d6e141ea7e26803');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ab3200a6fef743c1d328');
